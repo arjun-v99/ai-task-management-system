ec5f28ee9990091e09c2fa8cb2391c181d9347fc
Arjun Raj <arjunrajvilakkat@outlook.com>
2026-03-12 22:39:10 +0530

    added role column for users table

---
+++ database/migrations/2026_03_12_170640_alter_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admin', 'user'])->nullable()->after('email'); // Add a nullable 'votes' column after 'name'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role'); // Reverse the operation by dropping the column
+        });
+    }
+};
